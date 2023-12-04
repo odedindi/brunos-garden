1b4b5667bff786347bedeec9081f109639cf6cba
odi <odi@panter.ch>
2023-12-04 20:05:31 +0100

    feat: configure google api credentials

--- src/utils/getGoogleSheetApi.ts
+++ src/utils/getGoogleSheetApi.ts
@@ -2,6 +2,12 @@ import { google } from "googleapis"
 
 export const getGoogleSheetApi = async () => {
   const auth = await google.auth.getClient({
+    credentials: {
+      project_id: process.env.project_id,
+      private_key: process.env.private_key,
+      client_email: process.env.client_email,
+      client_id: process.env.client_id,
+    },
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   })
 
