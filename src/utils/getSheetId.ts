5e845d70e19c3a9320e4a22db9e720e0fd5707f0
odi <odi@panter.ch>
2023-12-04 17:50:47 +0100

    feat: set production sheet id to be the user email address

--- src/utils/getSheetId.ts
+++ src/utils/getSheetId.ts
@@ -1,4 +1,8 @@
 import dotenv from "dotenv"
+
 dotenv.config({ override: true })
 
-export const getSheetId = (email: string) => `${process.env.ENV_SHORT}-${email}`
+export const getSheetId = (email: string) =>
+  process.env.NODE_ENV === "production"
+    ? email
+    : `${process.env.ENV_SHORT}-${email}`
